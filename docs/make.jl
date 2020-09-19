2fb7986dc05166df1a79cb8696799107f15e9b97
John Washbourne <jwashbourne@chevron.com>
2020-09-19 21:40:45 +0000

    update docs/make.jl to add index and '.git' to repo name

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,14 @@
 using Documenter, Jets, LinearAlgebra, Random
 
-makedocs(sitename="Jets.jl", modules=[Jets])
+makedocs(
+    sitename="Jets.jl",
+    modules=[Jets]
+    pages = [
+        "index.md",
+        "reference.md"
+        ]
+)
 
 deploydocs(
-    repo = "github.com/ChevronETC/Jets.jl"
+    repo = "github.com/ChevronETC/Jets.jl.git"
 )
